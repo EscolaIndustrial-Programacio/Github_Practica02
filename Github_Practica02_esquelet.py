66bf8c77655b0728d192d8f1f170f96ba291d8c8
jbergada <jbergada@xtec.cat>
2022-11-30 18:28:05 +0100

    First commit

---
+++ Github_Practica02_esquelet.py
@@ -0,0 +1,215 @@
+#Bloc02 ->Python Github_Practica01.py  V03
+
+# Importar la llibreria math i la constant PI Alumn@ 26 Isidro P
+# L'alumn@ 26 ens explicarà què és la llibreria math i perquè la fem servir
+
+
+
+# Àrees i perímetres
+# Els números es corresponen amb el número que he assignat a cada figura de la taula
+# Els números també es corresponen amb els de l'alumn@ que ha de fer el programa i enviar-lo
+# El número de l'alumnat es correspon amb el seu número de llista
+# L'alumnat, un cop acabat el seu enviament, ajudarà al grup
+
+def quadrat(): # alumn@ 1 Arnau A
+  
+  
+def triangle(): # alumn@ 2 Pau A
+  
+  
+def rectangle(): # alumn@ 3 Oriol A
+ 
+ 
+def paralellogram(): # alumn@ 4 Alex A
+  
+  
+def rombe(): # alumn@ 5 Adam O
+   
+   
+def estel(): # alumn@ 6 Biel B
+  
+  
+def trapezi(): # alumn@ 7 Mariona B
+    
+
+def cercle(): # alumn@ 8 Eric C
+
+
+def poligon(): # alumn@ 9 Pol C
+    
+
+def corona(): # alumn@ 10 Judit C
+    
+    
+def sector(): # alumn@ 11 Marc C
+        
+
+# Àrees i volums
+
+def cub(): # alumn@ 12 Imran E
+    
+    
+def cilindre(): # alumn@ 13 Joan F
+    
+    
+def ortoedre(): # alumn@ 14 Pau F
+    
+
+def prisma_recte(): # alumn@ 15 Adrián de G
+    
+
+def con(): # alumn@ 16 Ilyas G
+    
+   
+def tronc_con(): # alumn@ 17 AlexandraElena G
+   
+
+def esfera(): # alumn@ 18 Iván J
+    
+
+def piramide(): # alumn@ 19 Isolda J
+    
+    
+def tetraedre_regular(): # alumn@ 20 Lluc J
+   
+
+def octaedre_regular(): # alumn@ 21 Oriol M
+    
+
+def tronc_piramide(): # alumn@ 22 Irina M
+    
+ 
+def casquet_esferic(): # alumn@ 23 Pere M
+    
+    
+def fus_falcaEsferica(): # alumn@ 24 Alex N
+    
+    
+def segment_esferic(): # alumn@ 25 Eduardo O
+    
+    
+
+# Els alumnes 27 i 28 buscaran les taules a treballar i comprobaran resultats d'execució
+
+# Programa principal  Alumn@ 27 OscarAlejandro P
+
+print(" Menú Mides en cm , cm2 i cm3   ")
+print("================================")
+print("")
+print("1.  ")
+print("2.  ")
+print("3. ")
+print("4. ")
+print("5. ")
+print("6.  ")
+print("7. ")
+print("8. ")
+print("9.  ")
+print("10.  ")
+print("11.  ")
+print("")
+print("12. ") # alumn@ 28 Pau R
+print("13.  ")
+print("14.  ")
+print("15. ")
+print("16. ")
+print("17.  ")
+print("18.  ")
+print("19. ")
+print("20.  ")
+print("21.  ")
+print("22.  ")
+print("23.  ")
+print("24. ")
+print("25.  ")
+print("")
+print("==============================================")
+
+menu = int(input("escull un element del menú: "))
+
+# Àrees i perímetres  alumn@ 29 Jan R
+
+if menu == 1 :
+   
+    
+elif menu == 2 :
+   
+    
+elif menu == 3 :
+    
+    
+elif menu == 4 : # alumn@ 30 Anaís S
+    
+    
+elif menu == 5 :
+    
+    
+elif menu == 6 :
+    
+    
+elif menu == 7 :  
+     
+    
+elif menu == 8 : # alumn@ 31 Adrià S
+    
+    
+elif menu == 9 :
+    
+
+elif menu == 10 :
+    
+    
+elif menu == 11 :
+    
+
+# Àrees i volums alumn@ 32 Jan S
+
+elif menu == 12 :
+    
+    
+elif menu == 13 :
+   
+    
+elif menu == 14 :
+    
+elif menu == 15 :
+
+    
+elif menu == 16 : # alumn@ 33 Diego V
+    
+    
+elif menu == 17 :
+    
+    
+elif menu == 18 :
+    
+    
+elif menu == 19 :
+    
+    
+elif menu == 20 :
+   
+    
+elif menu == 21 : # alumn@ 34 Anna W
+  
+  
+elif menu == 22 :
+    
+    
+elif menu == 23 :
+    
+    
+elif menu == 24 :
+    
+    
+elif menu == 25 :
+    
+
+# Git commands alumn@ 35 Wei Y
+
+
+""" Prèviament cada alumn@ haurà programat algunes de les figures
+primer amb programació estructurada i després amb programació modular.
+
+Cada alumn@ comprobarà la part de codi que envia des del seu ordinador local
+"""
